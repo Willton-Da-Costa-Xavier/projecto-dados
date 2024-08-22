8bba40f473e4b04a5f4b3d0964f38f18131f3f49
Willton <willtondacosta@gmail.com>
2024-08-22 18:24:25 +0200

    atualizei o layout da pagina de login

---
+++ public/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="shortcut icon" href="../app/img/favicon.ico" type="image/x-icon">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+       
+        <img src="../app/img/cadiadoChave.png" alt="cadiado e chave" class="chave">
+
+        <div class="formulario">
+            <form action="" method="post" autocomplete="off">
+                <fieldset>
+                    <legend>Nome</legend>
+                    <input type="text" name="nome" id="idnome">
+                </fieldset>
+                <fieldset>
+                    <legend>Senha</legend>
+                    <input type="password" name="pass" id="idpass">
+                </fieldset>
+                <input type="submit" value="Logar" class="btn">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
